eacc9b0c468e3d43d72631e0e387c0b1732fdaea
sturiot <turiot.sebastien@gmail.com>
2023-09-11 22:42:52 +0000

    seldon v2 protocol

---
+++ conf/seldon/deploy_sentiment_analysis_v2.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+sudo kubectl create namespace seldon-kedro
+
+sudo kubectl apply -f - << END
+
+apiVersion: machinelearning.seldon.io/v1alpha2
+kind: SeldonDeployment
+metadata:
+  name: sentiment-analysis
+  namespace: seldon-kedro
+spec:
+  protocol: kfserving  # Activate v2 protocol
+  name: sentiment-analysis-deployment
+  predictors:
+  - componentSpecs:
+    - spec:
+        containers:
+        - name: sentiment-analysis-pipeline-predict
+          image: sturiot/sturiotio:sentiment-analysis-v1.0.0
+    graph:
+      children: []
+      endpoint:
+        type: REST
+      name: sentiment-analysis-pipeline-predict
+      type: MODEL
+    name: sentiment-analysis-pipeline-predictor
+    replicas: 1
+
+END
